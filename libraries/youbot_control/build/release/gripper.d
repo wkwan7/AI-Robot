efb777cf3a2e8f1d302f512f4895df9226f8851f
wkwan7 <1920569859@qq.com>
2022-01-01 16:41:47 +0800

    init world

---
+++ libraries/youbot_control/build/release/gripper.d
@@ -0,0 +1,7 @@
+build/release/gripper.o: src/gripper.c include/gripper.h \
+ C:\Users\Henry\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
+ C:\Users\Henry\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\Henry\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\Henry\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ include/tiny_math.h \
+ C:\Users\Henry\AppData\Local\Programs\Webots/include/controller/c/webots/types.h
---
+++ libraries/youbot_control/build/release/tiny_math.d
@@ -0,0 +1,2 @@
+build/release/tiny_math.o: src/tiny_math.c include/tiny_math.h \
+ C:\Users\Henry\AppData\Local\Programs\Webots/include/controller/c/webots/types.h
